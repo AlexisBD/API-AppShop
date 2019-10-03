8d5cfd8da065ccd9cd089278efdca727d73c5dff
Milhernandez08 <milton30choko@hotmail.com>
2019-10-03 02:20:56 -0500

    Seed

--- seeder.py
+++ seeder.py
@@ -6,13 +6,12 @@ fake = Faker()
 
 url = 'http://192.168.0.16:8000/api/registration/'
 for _ in range(3):
-    username  = fake.name()
-    username  = username.split()
+    username  = fake.first_name()
     email     = fake.email()
     password1 = fake.password()
     password2 = password1
 
-    payload = {'username' : username[0], 'email' : email, 'password1' : password1, 'password2' : password2}
+    payload = {'username' : username, 'email' : email, 'password1' : password1, 'password2' : password2}
 
     print(payload)
     r = Request(url, urlencode(payload).encode())
