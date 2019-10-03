17758d33bd22f1111acd4e8a5edda290add5899d
Milhernandez08 <milton30choko@hotmail.com>
2019-10-03 05:41:47 -0500

    Listo

--- apps/sales/operaciones.py
+++ apps/sales/operaciones.py
@@ -7,20 +7,25 @@ class Operaciones ():
         self.Sal = Sale
 
     def residuo (self):
-        return int(self.Inv['quantity']) - int(self.Sal['quantity'])
+        if int(self.Sal['status']) != 0:
+            return int(self.Inv['quantity']) - int(self.Sal['quantity'])
+        else:
+            return int(self.Inv['quantity']) + int(self.Sal['quantity'])
 
     def subtotal (self):
-        total = int(self.Sal['quantity']) * float(self.Inv['price'])
-        descuento = (total * float(self.Sal['discount']))/100
-        return total - descuento
+        if int(self.Sal['status']) != 0:
+            total = int(self.Sal['quantity']) * float(self.Inv['price'])
+            descuento = (total * float(self.Sal['discount']))/100
+            return total - descuento
 
     def total (self):
-        iva = (self.subtotal() * float(self.Inv['tax']))/100
-        return self.subtotal() + iva
+        if int(self.Sal['status']) != 0:
+            iva = (self.subtotal() * float(self.Inv['tax']))/100
+            return self.subtotal() + iva
 
     def res (self):
         resultado = []
-        resultado.append(self.residuo)
-        resultado.append(self.subtotal)
-        resultado.append(self.total)
+        resultado.append(self.residuo())
+        resultado.append(self.subtotal())
+        resultado.append(self.total())
         return resultado
